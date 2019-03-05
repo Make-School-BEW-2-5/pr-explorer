065a2994a41b88ce66e58c5d91b6468c765ea428
Severiano Badajoz <sevebadajoz@gmail.com>
2019-03-05 13:40:57 -0800

    yeet

--- database/sqlite.go
+++ database/sqlite.go
@@ -18,22 +18,3 @@ func Init() (*gorm.DB, error) {
 
 	return db, nil
 }
-
-// func SaveData(data models.Query) {
-// 	db, err := Init()
-
-// 	for stargazer := range data.Repository.Stargazers {
-// 		company := strings.ToLower(strings.Join(strings.Fields(Stargazer.Company), ""))
-// 		if company == "makeschool" {
-// 			user := &models.User{Login: stargazer.Login, PullRequests: make([]models.PullRequest, len(stargazer.Nodes.PullRequests.Nodes))}
-// 			for node := range stargazer.Nodes.PullRequests.Nodes {
-// 				pullRequest := &models.PullRequest{Title: node.Title, Date: node.createdAt}
-// 				db.NewRecord(pullRequest)
-// 				db.Create(&pullRequest)
-// 				append(user.PullRequests, pullRequest)
-// 			}
-// 			db.NewRecord(user)
-// 			db.Create(&user)
-// 		}
-// 	}
-// }
