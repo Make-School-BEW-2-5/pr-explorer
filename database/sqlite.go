cd414a86a8dcea61d30f0873d249dab9a709e053
Severiano Badajoz <sevebadajoz@gmail.com>
2019-03-05 13:36:16 -0800

    s/o to jayce

--- database/sqlite.go
+++ database/sqlite.go
@@ -1,8 +1,6 @@
 package database
 
 import (
-	"strings"
-
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 
@@ -21,21 +19,21 @@ func Init() (*gorm.DB, error) {
 	return db, nil
 }
 
-func SaveData(data models.Query) {
-	db, err := Init()
+// func SaveData(data models.Query) {
+// 	db, err := Init()
 
-	for stargazer := range data.Repository.Stargazers {
-		company := strings.ToLower(strings.Join(strings.Fields(Stargazer.Company), ""))
-		if company == "makeschool" {
-			user := &models.User{Login: stargazer.Login, PullRequests: make([]models.PullRequest, len(stargazer.Nodes.PullRequests.Nodes))}
-			for node := range stargazer.Nodes.PullRequests.Nodes {
-				pullRequest := &models.PullRequest{Title: node.Title, Date: node.createdAt}
-				db.NewRecord(pullRequest)
-				db.Create(&pullRequest)
-				append(user.PullRequests, pullRequest)
-			}
-			db.NewRecord(user)
-			db.Create(&user)
-		}
-	}
-}
+// 	for stargazer := range data.Repository.Stargazers {
+// 		company := strings.ToLower(strings.Join(strings.Fields(Stargazer.Company), ""))
+// 		if company == "makeschool" {
+// 			user := &models.User{Login: stargazer.Login, PullRequests: make([]models.PullRequest, len(stargazer.Nodes.PullRequests.Nodes))}
+// 			for node := range stargazer.Nodes.PullRequests.Nodes {
+// 				pullRequest := &models.PullRequest{Title: node.Title, Date: node.createdAt}
+// 				db.NewRecord(pullRequest)
+// 				db.Create(&pullRequest)
+// 				append(user.PullRequests, pullRequest)
+// 			}
+// 			db.NewRecord(user)
+// 			db.Create(&user)
+// 		}
+// 	}
+// }
